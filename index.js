f62e2ceecd621e344e725d2fbd6a5674a54d34fa
lilchahat <chahat.rathor@alumni.sifal.deerwalk.edu.np>
2026-06-24 12:10:17 +0545

    fix: update welcome message for clarity in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.get("/", (req, res) => {
   const obj = lodash.cloneDeep({
     message:
-      "Hello, DevSecOps by Chahat RS Rathour!, Welcome to the world of secure coding and DevSecOps practices.",
+      "Hello, DevSecOps by Chahat RS Rathour!, Welcome to the world of secure coding and security practices.",
   });
 
   const pattern = minimatch(".js", ".js");
