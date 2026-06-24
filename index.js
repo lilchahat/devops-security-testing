3797527d3d62807b5a43bc32d01bf6290ae81fb5
lilchahat <chahat.rathor@alumni.sifal.deerwalk.edu.np>
2026-06-24 11:49:08 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const lodash = require("lodash");
+const minimatch = require("minimatch");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const obj = lodash.cloneDeep({
+    message:
+      "Hello, DevSecOps by Chahat RS Rathour!, Welcome to the world of secure coding and DevSecOps practices.",
+  });
+
+  const pattern = minimatch(".js", ".js");
+
+  res.send(`${obj.message} (Pattern match: ${pattern})`);
+});
+
+module.exports = app;
